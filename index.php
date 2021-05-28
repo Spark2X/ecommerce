7bccba43a743854e2102b5819149cad551773158
Felipe Silveira <fesilveira2000@gmail.com>
2021-05-28 15:56:37 -0300

    crud 1.0

--- index.php
+++ index.php
@@ -56,6 +56,77 @@ $app->get('/admin/logout', function() {
 
 });
 
+$app->get("/admin/users", function(){
+
+	User::verifylogin();
+
+	$users = User::listAll();
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get("/admin/users/create", function(){
+
+	User::verifylogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifylogin();
+	
+
+});
+
+
+$app->get("/admin/users/:iduser", function($iduser){
+
+	User::verifylogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("users-update");
+
+});
+
+$app->post("/admin/users/create", function () {
+
+	User::verifyLogin();
+
+    $user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+		"cost"=>12
+
+	]);
+
+	$user->setData($_POST);
+
+    $user->save();
+
+
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifylogin();
+	
+
+});
+
+
 $app->run();
 
- ?>
+?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -52,6 +52,40 @@ class User extends Model {
 
 	}
 
+	public static function listAll(){ 
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	public function save(){ 
+
+		$sql = new Sql();
+		/*
+		pqdesperson VARCHAR(64),
+		pdeslogin VARCHAR(64),
+		pdespassword VARCHAR(256),
+		pdesemal VARCHAR(128),
+		pnrphone BIGINT,
+		pinadmin TINYINT,
+		*/
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin", 
+		
+			array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+		var_dump($results);
+		echo "<hr>";
+		$this->setData($results);
+		var_dump($results);
+	}
+
 	public static function verifyLogin($inadmin = true)
 	{
 
